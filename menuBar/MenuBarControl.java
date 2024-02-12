7bcc4071ad0c0f9404ba35765d3af840cdfae9f0
Satya3020 <92669492+Satya3020@users.noreply.github.com>
2024-02-12 23:55:43 +0530

    Update MenuBarControl.java

--- menuBar/MenuBarControl.java
+++ menuBar/MenuBarControl.java
@@ -1,1 +1,15 @@
+package menuBar;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.io.IOException;
+public class MenuBarControl {
+    public void close(){
+        Platform.exit();
+        System.exit(0);
+    }
+}
 
