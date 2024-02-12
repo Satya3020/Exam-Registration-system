4a807713b3f6244e9b68d41a89d4e64ec5025619
Satya3020 <92669492+Satya3020@users.noreply.github.com>
2024-02-13 00:01:51 +0530

    Update Course.sql

--- sqlsheet/Course.sql
+++ sqlsheet/Course.sql
@@ -1,1 +1,8 @@
+CREATE TABLE `course` (  `dbCourseSerial` int(11) NOT NULL AUTO_INCREMENT,
+  `dbCourseSec` varchar(64) DEFAULT NULL,
+  `dbCourseCredit` int(11) DEFAULT NULL,
+  `dbCourseCode` varchar(64) DEFAULT NULL,
+  `dbCourseTitle` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`dbCourseSerial`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
