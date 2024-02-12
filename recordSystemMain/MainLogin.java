409f4d9a2fac3cffb286ce14a276fb85c67f079b
Satya3020 <92669492+Satya3020@users.noreply.github.com>
2024-02-12 23:59:06 +0530

    Update MainLogin.java

--- recordSystemMain/MainLogin.java
+++ recordSystemMain/MainLogin.java
@@ -1,1 +1,20 @@
+package recordSystemMain;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class MainLogin extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainLogin.fxml"));
+        primaryStage.setTitle("Exam Registration System");
+        primaryStage.setScene(new Scene(root, 650, 400));
+        primaryStage.show();
+    }
+}
+
 
