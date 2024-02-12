42117729a1aabbb4f497917744839d71fac2591d
Satya3020 <92669492+Satya3020@users.noreply.github.com>
2024-02-12 23:54:49 +0530

    Update DBConnection.java

--- database/DBConnection.java
+++ database/DBConnection.java
@@ -1,1 +1,17 @@
+package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private Connection connection;
+    private String url = "jdbc:mysql://localhost:3307/uiuStudentRecordSystem";
+    private String user = "root";
+    private String pass = "";
+    public Connection getConnection() throws SQLException {
+        connection = DriverManager.getConnection(url,user,pass);
+        return connection;
+    }
+}
+
 
